d055f257a6463ca2073c4e06c4747e8ba671498a
Maksim <dollydasheep@gmail.com>
2015-08-24 17:23:25 -0400

    Add more ajax functions
    Added loginUserRequest and updateUserRequest ajax functions to ajax.js
    Need additional elementId data and testing.

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -2,9 +2,9 @@
 var sa = 'http://localhost:8000';
 
 
-var registerRequest = function (){
-  var formData = {
-    email: $('#register-email').val(),
+var registerUserRequest = function (){
+  var registerData = {
+    username: $('#register-email').val(),
     password: $('#register-password').val(),
     firstName: $('#register-firstName').val(),
     lastName: $('#register-lastName').val(),
@@ -15,7 +15,7 @@ var registerRequest = function (){
     type: 'POST',
     contentType: 'application/json',
     processData: false,
-    data: formData
+    data: JSON.stringify(registerData)
   })
   .done(function(data) {
     // $('#result').html(JSON.stringify(data, null, 2));
@@ -25,5 +25,53 @@ var registerRequest = function (){
     console.error(jqxhr);
     // update later
   });
-
 };
+
+var loginUserRequest = function (){
+  var loginData = {
+    username: $('#login-email').val(),
+    password: $('#login-password').val()
+  };
+  $.ajax({
+    url: sa + '/login',
+    type: 'POST',
+    contentType: 'application/json',
+    processData: false,
+    data: JSON.stringify(loginData)
+  })
+  .done(function(data){
+    // back to the homepage, keeping the session alive
+  })
+  .fail(function(jqxhr) {
+    console.error(jqxhr);
+  });
+};
+
+var updateUserRequest = function(){
+  vat updateDate = {
+    username: $('#update-email').val(),
+    password: $('#update-password').val(),
+    firstName: $('#update-firstName').val(),
+    lastName: $('#update-lastName').val(),
+    company: $('#update-company').val()
+  };
+  $.ajax({
+    url: sa + '/users/', // CLARIFY IT
+    type: 'POST',
+    contentType: 'application/json',
+    processData: false,
+    data: JSON.stringify(loginData)
+  })
+  .done(function(data){
+    // back to the homepage, keeping the session alive
+  })
+  .fail(function(jqxhr) {
+    console.error(jqxhr);
+  });
+};
+
+
+// changePasswordRequest, , deleteUserRequest, , logoutUserRequest
+// indexFilesRequest, deleteFileRequest, updateFileRequest, uploadFileRequest
+
+// stretch: showFileRequest
