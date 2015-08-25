bca69b4dd44bacabc52ff7ea1f94b5c70aac5612
caraclarke <cara.clarke8@gmail.com>
2015-08-25 13:51:01 -0400

    Handlebars templates added
    Added handlebars templates on profile.html and beginning functions in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -12,4 +12,27 @@ $(document).ready(function(){
       registerUserRequest();
     });
 
+    $('#login-button').on('click', function(){
+      loginUserRequest();
+    });
+
+    $('#profile-page').on('click', function(){
+      showProfileRequest();
+    });
+
+
+  var showProfileForm = function (data) {
+    var profileShowTemplate = Handlebars.compile($('#profile-show-template').html());
+    $("#display-profile-information").html(profileShowTemplate(data));
+  };
+
+  var indexTagCloud = function (data) {
+    var tagIndexTemplate = Handlebars.compile($('#tag-index-template').html());
+    $('#display-tag-index').html(tagIndexTemplate(data));
+  };
+
+  $(".tag-cloud").on('click', 'h4 > a', function(event){
+    showFilesByTagRequest($(this).id);
+  });
+
 }); // end document.ready
