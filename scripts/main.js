f57a8359354fc655f5aa84cf9b5c8d453083568e
caraclarke <cara.clarke8@gmail.com>
2015-08-26 14:43:32 -0400

    Upload form function added
    Upload form function added but not tested

--- scripts/main.js
+++ scripts/main.js
@@ -45,3 +45,27 @@ $(".tag-cloud").on('click', 'h4 > a', function(event){
   event.preventDefault();
   showElementsByTagRequest($(this).id);
 });
+
+// upload form function
+
+  $('#upload-form').on('submit', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
+  });
+  $('#upload').on('click', function (e) {
+    var formData = new FormData($('#upload-form')[0]);
+    $.ajax({
+      url: 'http://localhost:8000/images',
+      type: 'POST',
+      contentType: false,
+      processData: false,
+      data: formData
+    })
+    .done(function(data) {
+      $('#result').html(JSON.stringify(data, null, 2));
+    })
+    .fail(function(jqxhr) {
+      console.error(jqxhr);
+    });
+  });
