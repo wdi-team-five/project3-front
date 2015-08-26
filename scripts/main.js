5c15f605d30cf5449d8ed00709e38e42c3325bcb
caraclarke <cara.clarke8@gmail.com>
2015-08-26 19:31:41 -0400

    Handlebars tag cloud display
    Added helper so that tag cloud displays as row and cuts off every 5th tag.

--- scripts/main.js
+++ scripts/main.js
@@ -45,3 +45,19 @@ $(".tag-cloud").on('click', 'h4 > a', function(event){
   event.preventDefault();
   showElementsByTagRequest($(this).id);
 });
+
+// tag make row handlebars helper
+Handlebars.registerHelper('grouped_each', function(every, context, options) {
+  var out = "", subcontext = [], i;
+  if (context && context.length > 0) {
+    for (i = 0; i < context.length; i++) {
+      if (i > 0 && i % every === 0) {
+        out += options.fn(subcontext);
+        subcontext = [];
+      }
+      subcontext.push(context[i]);
+    }
+    out += options.fn(subcontext);
+  }
+  return out;
+});
