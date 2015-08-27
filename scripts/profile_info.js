180e062ee6bca837fb57fa844eb76b9cebbec824
caraclarke <cara.clarke8@gmail.com>
2015-08-26 20:05:46 -0400

    Changed profile information/tag cloud load
    Changed loading of profile information and tag cloud associated with user from button click to on profile page loads

---
+++ scripts/profile_info.js
@@ -0,0 +1,21 @@
+ $(document).ready(function(){
+
+  var showProfileForm = function (data) {
+    var profileShowTemplate = Handlebars.compile($('#profile-show-template').html());
+    $('#display-profile-information').html(profileShowTemplate(data));
+  };
+
+  var indexTagCloud = function (data) {
+    var tagIndexTemplate = Handlebars.compile($('#tag-index-template').html());
+    $('#display-tag-index').html(tagIndexTemplate(data));
+  };
+
+  var indexDocumentForm = function (data) {
+    var documentIndexTemplate = Handlebars.compile($('#document-index-template').html());
+    $('#display-document-index').html(documentIndexTemplate(data));
+  };
+  showProfileForm(testProfileData); // CHANGE TO DATA
+  indexTagCloud({tags: testTagData});
+  indexDocumentForm({documents: testFileData});
+
+ });
