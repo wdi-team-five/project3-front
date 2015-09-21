53dbeed2a7b3e3abd7cecad98cd7613f9e71d77c
caraclarke <cara.clarke8@gmail.com>
2015-09-21 12:17:18 -0400

    Chris changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,67 +1,68 @@
-var showProfileForm = function (data) {
-    var profileShowTemplate = Handlebars.compile($('#profile-show-template').html());
-    $('#display-profile-information').html(profileShowTemplate(data));
-  };
+'use strict';
+var showProfileForm = function(data) {
+	var profileShowTemplate = Handlebars.compile($('#profile-show-template').html());
+	$('#display-profile-information').html(profileShowTemplate(data));
+};
 
-  var indexTagCloud = function (data) {
-    var tagIndexTemplate = Handlebars.compile($('#tag-index-template').html());
-    $('#display-tag-index').html(tagIndexTemplate(data));
-    $(".tag-cloud").on('click', function(event){
-      showElementsByTagRequest($(this).html().toString());
-    });
-  };
+var indexTagCloud = function(data) {
+	var tagIndexTemplate = Handlebars.compile($('#tag-index-template').html());
+	$('#display-tag-index').html(tagIndexTemplate(data));
+	$(".tag-cloud").on('click', function(event) {
+		showElementsByTagRequest($(this).html().toString());
+	});
+};
 
-  var prependTagCloud = function (data) {
-    var prependTagTemplate = Handlebars.compile($('#tag-prepend-template').html());
-    $('#display-tag-index').prepend(prependTagTemplate(data));
-    $(".tag-cloud").on('click', function(event){
-      showElementsByTagRequest($(this).html().toString());
-    });
-  };
+var prependTagCloud = function(data) {
+	var prependTagTemplate = Handlebars.compile($('#tag-prepend-template').html());
+	$('#display-tag-index').prepend(prependTagTemplate(data));
+	$(".tag-cloud").on('click', function(event) {
+		showElementsByTagRequest($(this).html().toString());
+	});
+};
 
-  var indexDocumentForm = function (data) {
-    var documentIndexTemplate = Handlebars.compile($('#document-index-template').html());
-    $('#display-document-index').html(documentIndexTemplate(data));
-    $('.documentLink').on('mouseover', function() {
-      $('.deleteButtons').removeClass('hide');
-    });
-    $('.documentLink').on('mouseout', function() {
-      $('.deleteButtons').addClass('hide');
-    });
-    $('.deleteButtons').on('click',function(event){
-      deleteElementRequest($(this).attr('id'));//needs to be mongoID);
-      showFileRequest();
-      showTagsRequest();
-    });
-  };
+var indexDocumentForm = function(data) {
+	var documentIndexTemplate = Handlebars.compile($('#document-index-template').html());
+	$('#display-document-index').html(documentIndexTemplate(data));
+	$('.documentLink').on('mouseover', function() {
+		$('.deleteButtons').removeClass('hide');
+	});
+	$('.documentLink').on('mouseout', function() {
+		$('.deleteButtons').addClass('hide');
+	});
+	$('.deleteButtons').on('click', function(event) {
+		deleteElementRequest($(this).attr('id')); //needs to be mongoID);
+		showFileRequest();
+		showTagsRequest();
+	});
+};
 
-  var prependDocumentForm = function (data) {
-    var documentPrependTemplate = Handlebars.compile($('#document-prepend-template').html());
-    $('#display-document-index').prepend(documentPrependTemplate(data));
-  };
+var prependDocumentForm = function(data) {
+	var documentPrependTemplate = Handlebars.compile($('#document-prepend-template').html());
+	$('#display-document-index').prepend(documentPrependTemplate(data));
+};
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-    $("#show-register-modal").click(function(){
-        $("#register-modal").modal();
-    });
+	$("#show-register-modal").click(function() {
+		$("#register-modal").modal();
+	});
 
-    $("#show-login-modal").click(function(){
-        $("#login-modal").modal();
-    });
+	$("#show-login-modal").click(function() {
+		$("#login-modal").modal();
+	});
 
-    $('#register-button').on('click', function(){
-      registerUserRequest();
-    });
+	$('#register-button').on('click', function() {
+		registerUserRequest();
+	});
 
-    $('#login-button').on('click', function(){
-      loginUserRequest();
-    });
+	$('#login-button').on('click', function() {
+		loginUserRequest();
+	});
 
-  // hideButton();
-  showProfileRequest();
-  showFileRequest();
-  showTagsRequest();
+	// hideButton();
+	showProfileRequest();
+	showFileRequest();
+	showTagsRequest();
 
 }); // end document.ready
 
@@ -82,4 +83,3 @@ $(document).ready(function(){
 //   }
 //   return out;
 // });
-
